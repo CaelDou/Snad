494ea62fc975a16e24ac1a490f26a8e7c2adedad
Cael Doubrough <105995920+CaelDou@users.noreply.github.com>
2025-05-06 21:01:33 -0300

    Update compile.sh

--- oop_game/compile.sh
+++ oop_game/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Compiling...
 clear
-cd /home/cael/Desktop/C/oop_game/
+cd /home/cael/Desktop/C/g++/Snad/oop_game/
 #g++ -Wall main.cpp logo.cpp map.cpp -o build.sh
 g++ ray.cpp -lraylib -o build.sh
 sleep 1
